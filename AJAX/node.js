e4848f634d99f716cd52ba4819df4b98e1a96698
thuyen <20010928@st.phenikaa-uni.edu.vn>
2023-03-17 17:34:44 +0700

    m

---
+++ AJAX/node.js
@@ -0,0 +1,22 @@
+// function LoadXMLDoc(){
+
+//     req = new XMLHttpRequest();
+//     req.onreadystatechange = function (){
+//      if(this.readyState == 4 && this.status==200){
+//         console.log(333,JSON.parse(this.response));
+    
+//      }   
+//     };
+//     req.open("GET","https://jsonplaceholder.typicode.com/users",true)
+//     req.send()
+// }
+// LoadXMLDoc()
+// function fetchProducts(){
+//     const data = fetch("https://jsonplaceholder.typicode.com/users");
+//     data.then(function(response){
+//         response.json().then(function (res){
+//             console.log(111,res)
+//         });
+//     });
+// }
+// fetchProducts()
